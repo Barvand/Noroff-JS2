4984aa60608f561aba005ae503e394b38c084a19
B <128028346+Barvand@users.noreply.github.com>
2024-05-06 14:56:39 +0200

    user is able to update post

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,13 +1,16 @@
+// import * as constants from "./api/constants.mjs";
+
+// console.log(constants.API_SOCIAL_URL);
+
 import * as listeners from "./handlers/index.mjs";
 import * as postMethods from "./api/posts/index.mjs";
 import * as render from "./render/index.mjs";
-import * as search from "./filters/index.mjs";
 import { getProfiles } from "./api/profiles/read.mjs";
 import { renderCompleteProfile } from "./render/index.mjs";
 
 const path = location.pathname;
 
-const feedContainer = document.querySelector ("#feed-container")
+const feedContainer = document.querySelector("#feed-container");
 
 if (path === "/profile/register/") {
   listeners.setRegisterFormListener();
@@ -15,19 +18,19 @@ if (path === "/profile/register/") {
   listeners.setLoginFormListener();
 } else if (path === "/feed/post/create/") {
   listeners.setCreatePostFormListener();
-} 
+} else if (path === "/feed/post/edit/") {
+  listeners.setUpdatePostFormListener();
+}
 
 if (path === "/feed/") {
   render.renderAllPosts(feedContainer);
 } else if (path === "/feed/post/") {
   render.renderSinglePost();
 } else if (path === "/profiles") {
-  
 }
 
+// PROFILE Pathing
 
-// PROFILE Pathing 
-
-if (path === "/profile/") { 
+if (path === "/profile/") {
   renderCompleteProfile();
-} 
+}
