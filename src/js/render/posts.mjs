7df52450aebdbbd7c343d9bd8ac69114061155dc
B <128028346+Barvand@users.noreply.github.com>
2024-05-09 20:23:06 +0200

    revised whole render function

--- src/js/render/posts.mjs
+++ src/js/render/posts.mjs
@@ -1,125 +1,5 @@
 import { getPosts } from "../api/posts/read.mjs";
 
-export async function createPostsHTML(post, parentElement) {
-  // Create post card anchor element
-  const cardWrap = document.createElement("a");
-  cardWrap.classList.add("col-md-12", "col-lg-6", "mt-2", "mb-2");
-  cardWrap.href = `/feed/post/?id=${post.id}`;
-  parentElement.appendChild(cardWrap);
-
-  // Create post card div
-  const postCard = document.createElement("div");
-  postCard.classList.add("card", "my-custom-card", "h-100");
-  cardWrap.appendChild(postCard);
-
-  // Flex container for the avatar and the name of the profile.
-  const avatarFlexContainer = document.createElement("div");
-  avatarFlexContainer.classList.add(
-    "d-flex",
-    "p-1",
-    "align-items-center",
-    "name-tag",
-    "third-color"
-  );
-  postCard.appendChild(avatarFlexContainer);
-
-  // Avatar of the user
-  const postAvatar = document.createElement("img");
-  postAvatar.src = post.author.avatar;
-  postAvatar.href = post.author.name;
-  postAvatar.classList.add(
-    "rounded-circle",
-    "border-secondary",
-    "border",
-    "border-2",
-    "profile-picture-posts"
-  );
-  avatarFlexContainer.appendChild(postAvatar);
-
-  // Author
-  const postAuthor = document.createElement("a");
-  postAuthor.innerText = `@${post.author.name}`;
-  postAuthor.classList.add("text-white", "fw-bold");
-  postAuthor.href = `/profile/?name=${post.author.name}`;
-  avatarFlexContainer.appendChild(postAuthor);
-
-  // Creates the image container 
-  const imageContainer = document.createElement("div");
-  imageContainer.classList.add("post-image-container");
-  postCard.appendChild(imageContainer);
-
-  // Create the image post
-  const postImage = document.createElement("img");
-  postImage.classList.add("card-img-top");
-  postImage.alt = post.title;
-
-  // Check if post has media
-  if (post.media) {
-    postImage.src = post.media;
-  } else {
-    // If no media available, set a default picture
-    postImage.src = "/images/dogpost.jpg"; // Replace "default-image.jpg" with your default image URL
-  }
-
-  imageContainer.appendChild(postImage);
-
-
-  // Post tags
-  const postTags = document.createElement("p");
-  postTags.innerText = `#${post.tags}`;
-  postTags.classList.add("border", "m-0", "post-tags");
-  imageContainer.appendChild(postTags);
-
-  // The body of the card
-  const cardBody = document.createElement("div");
-  cardBody.classList.add("card-body", "pt-0");
-  postCard.appendChild(cardBody);
-
-  // Creates the title of the post
-  const postTitle = document.createElement("h2");
-  postTitle.classList.add(
-    "display-6",
-    "fw-bold",
-    "fst-italic",
-    "text-capitalize",
-    "fs-5"
-  );
-  postTitle.innerText = post.title;
-  cardBody.appendChild(postTitle);
-
-
-  // post body text. 
-  const postBody = document.createElement("p");
-  postBody.innerText = post.body;
-  postBody.classList.add("fs-6");
-  cardBody.appendChild(postBody);
-
-  const timestamp = new Date(post.created);
-  const day = timestamp.getDate();
-  const month = timestamp.getMonth() + 1; // Adding 1 because months are zero-based
-  const year = timestamp.getFullYear();
-
-  const formattedDate = `${day}/${month}/${year}`;
-
-  const postDate = document.createElement("p");
-  postDate.innerText = `Posted on ` + formattedDate;
-  postDate.classList.add("text-muted", "fst-italic");
-  cardBody.appendChild(postDate);
-
-  const divElement = document.createElement("div");
-  divElement.classList.add("d-flex", "justify-content-end");
-  cardBody.appendChild(divElement);
-
-  const postComments = document.createElement("p");
-  postComments.innerHTML = `<i class="fa-sharp fa-regular fa-heart" aria-hidden="true"> ${post._count.comments} </i>`;
-  divElement.appendChild(postComments);
-
-  const postReactions = document.createElement("p");
-  postReactions.innerHTML = `<i class="fa-regular fa-comment" aria-hidden="true"> ${post._count.reactions} </i>`;
-  divElement.appendChild(postReactions);
-};
-
-
 export async function renderAllPosts(parentElement) {
   try {
 
@@ -133,6 +13,7 @@ export async function renderAllPosts(parentElement) {
     console.error("Error rendering posts:", error);
   }
 };
+
 
 
 
@@ -170,4 +51,179 @@ export async function createProfileData(profiles, parentElement) {
       profileAvatar.src = "/images/dogpost.jpg"; // Replace "default-image.jpg" with your default image URL
     }
   });
+}
+
+
+
+
+async function renderCompletePost(post, parentElement) {
+  // Create post card anchor element
+  const cardWrap = document.createElement("div");
+  cardWrap.classList.add("col-md-12", "col-lg-6", "mt-2", "mb-2", "mx-auto");
+  parentElement.appendChild(cardWrap);
+
+  // Render post username and image
+  renderPostUsernameAndImage(post, cardWrap);
+
+  // Render post image
+  renderPostImage(post, cardWrap);
+
+  // Render post tags
+  renderPostTags(post, cardWrap);
+
+  // Render post card
+ renderPostCard(post, cardWrap);
+}
+
+
+async function renderPostUsernameAndImage(post, parentElement) {
+// the flex container to get the 2 items next to eachother. 
+const avatarFlexContainer = document.createElement("div");
+avatarFlexContainer.classList.add(
+  "d-flex",
+  "p-1",
+  "align-items-center",
+  "name-tag",
+  "third-color"
+);
+parentElement.appendChild(avatarFlexContainer);
+
+// Avatar of the user
+const postAvatar = document.createElement("img");
+postAvatar.src = post.author.avatar;
+postAvatar.href = post.author.name;
+postAvatar.classList.add(
+  "rounded-circle",
+  "border-secondary",
+  "border",
+  "border-2",
+  "profile-picture-posts"
+);
+avatarFlexContainer.appendChild(postAvatar);
+
+// the name next to the avatar
+const postAuthor = document.createElement("a");
+postAuthor.innerText = `@${post.author.name}`;
+postAuthor.classList.add("text-white", "fw-bold");
+postAuthor.href = `/profile/?name=${post.author.name}`;
+avatarFlexContainer.appendChild(postAuthor);
+
+return avatarFlexContainer;
+} 
+
+
+async function renderPostImage(post, parentElement) {
+  const anchorTag = document.createElement("a"); 
+  anchorTag.href = `/feed/post/?id=${post.id}`;
+
+  // Create the image for the post
+  const postImage = document.createElement("img");
+  postImage.classList.add("card-img-top");
+  postImage.alt = post.title;
+
+  // Check if post has media
+  if (post.media) {
+    postImage.src = post.media;
+  } else {
+    // If no media available, set a default picture
+    postImage.src = "/images/dogpost.jpg"; // Replace "default-image.jpg" with your default image URL
+  }
+
+  parentElement.appendChild(anchorTag)
+  anchorTag.appendChild(postImage);
+
+  return anchorTag;
+}
+
+async function renderPostTags(post, parentElement) {
+  // Post tags
+  const postTags = document.createElement("p");
+  postTags.innerText = `#${post.tags}`;
+  postTags.classList.add("border", "m-0", "post-tags");
+  parentElement.appendChild(postTags);
+  return postTags;
+}
+
+async function renderPostCard(post, parentElement) {
+    // The body of the card
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body", "p-1");
+    parentElement.appendChild(cardBody);
+
+    // Render post title
+    renderPostTitle(post, cardBody);
+
+    // Render post body
+    renderPostBody(post, cardBody);
+
+    // Render post date
+    renderPostDate(post, cardBody);
+
+    // Render comments and reactions
+    renderCommentsAndReactions(post, cardBody);
+
+    return cardBody;
+}
+
+
+async function renderPostTitle(post, parentElement) {
+  // Creates the title of the post
+  const postTitle = document.createElement("h2");
+  postTitle.classList.add(
+    "display-6",
+    "fw-bold",
+    "fst-italic",
+    "text-capitalize",
+    "fs-5"
+  );
+  postTitle.innerText = post.title;
+  parentElement.appendChild(postTitle);
+  return postTitle
+}
+
+async function renderPostBody(post, parentElement) {
+  const postBody = document.createElement("p");
+  postBody.innerText = post.body;
+  postBody.classList.add("fs-6");
+  parentElement.appendChild(postBody);
+
+  return postBody
+
+}
+
+async function renderPostDate(post, parentElement) { 
+const timestamp = new Date(post.created);
+const day = timestamp.getDate();
+const month = timestamp.getMonth() + 1; // Adding 1 because months are zero-based
+const year = timestamp.getFullYear();
+
+const formattedDate = `${day}/${month}/${year}`;
+
+const postDate = document.createElement("p");
+postDate.innerText = `Posted on ` + formattedDate;
+postDate.classList.add("text-muted", "fst-italic");
+parentElement.appendChild(postDate);
+
+return postDate
+}
+ 
+async function renderCommentsAndReactions(post, parentElement){ 
+  const divElement = document.createElement("div");
+  divElement.classList.add("d-flex", "justify-content-end");
+  parentElement.appendChild(divElement);
+
+  const postComments = document.createElement("p");
+  postComments.innerHTML = `<i class="fa-sharp fa-regular fa-heart" aria-hidden="true"> ${post._count.comments} </i>`;
+  divElement.appendChild(postComments);
+
+  const postReactions = document.createElement("p");
+  postReactions.innerHTML = `<i class="fa-regular fa-comment" aria-hidden="true"> ${post._count.reactions} </i>`;
+  divElement.appendChild(postReactions);
+
+  return divElement
+}
+
+
+export async function createPostsHTML(post, parentElement) { 
+renderCompletePost(post, parentElement);
 }
